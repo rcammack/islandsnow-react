fbc35d54536760943a31e3d844ff0d7d6eeed7e4
rcammack <rcammack@hawaii.edu>
2018-02-25 20:20:26 -1000

    Task 4

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,17 +2,136 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 import 'semantic-ui-css/semantic.min.css';
-import { Container, Header } from 'semantic-ui-react';
+import { Container, Header, Menu, Icon, Dropdown, Image, Grid, Divider, List, Input } from 'semantic-ui-react';
+
+class TopMenu extends React.Component {
+  render() {
+    return (
+        <Menu borderless className="topMenu">
+          <Container>
+            <Menu.Item fitted><Icon name="facebook f"/></Menu.Item>
+            <Menu.Item fitted><Icon name="twitter"/></Menu.Item>
+            <Menu.Item fitted><Icon name="pinterest"/></Menu.Item>
+            <Menu.Item fitted><Icon name="instagram"/></Menu.Item>
+            <Menu.Item fitted position="right"><Icon name="home"/></Menu.Item>
+            <Menu.Item fitted><Icon name="search"/></Menu.Item>
+            <Menu.Item fitted><Icon name="user"/></Menu.Item>
+            <Dropdown item text="MY CART 0" icon="shop">
+              <Dropdown.Menu>
+                <Dropdown.Item>My cart is currently empty.</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Container>
+        </Menu>
+    )
+  }
+}
+
+class IslandSnowLogo extends React.Component {
+  render() {
+    return (
+        <Container>
+          <Image centered
+                 src="https://cdn.shopify.com/s/files/1/1035/5187/t/5/assets/logo.png?15691486226381689305"/>
+        </Container>
+    )
+  }
+}
+
+class MiddleMenu extends React.Component {
+  render() {
+    return (
+        <Menu borderless className="mainMenu">
+          <Container>
+            <Dropdown item text="MEN" icon="dropdown">
+              <Dropdown.Menu>
+                <Dropdown.Item>shirts</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+
+            <Dropdown item text="WOMEN" icon="dropdown">
+              <Dropdown.Menu>
+                <Dropdown.Item>shirts</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+
+            <Menu.Item>KIDS</Menu.Item>
+
+            <Dropdown item text="BRANDS" icon="dropdown">
+              <Dropdown.Menu>
+                <Dropdown.Item>shirts</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+
+            <Menu.Item>SEARCH</Menu.Item>
+          </Container>
+        </Menu>
+    )
+  }
+}
+
+class FullWidthImage extends React.Component {
+  render() {
+    return (
+        <Image fluid src="https://cdn.shopify.com/s/files/1/1035/5187/t/5/assets/slide1.jpg?15691486226381689305"/>
+    )
+  }
+}
+
+class FooterMenu extends React.Component {
+  render() {
+    return (
+        <div className="grey-background">
+          <Container>
+            <Grid columns={3}>
+              <Grid.Column>
+                NAVIGATION
+                <hr/>
+                <List>
+                  <List.Item>About Us</List.Item>
+                  <List.Item>Videos</List.Item>
+                  <List.Item>Store Locations</List.Item>
+                </List>
+              </Grid.Column>
+
+              <Grid.Column>
+                MAIN MENU
+                <hr/>
+                <List>
+                  <List.Item>Men</List.Item>
+                  <List.Item>Women</List.Item>
+                  <List.Item>Kids</List.Item>
+                </List>
+              </Grid.Column>
+
+              <Grid.Column>
+                CONNECT
+                <hr/>
+                <div className="text">Sign up for the latest updates</div>
+                <Input placeholder="Enter email address" label="join" labelPosition="right"/>
+              </Grid.Column>
+            </Grid>
+          </Container>
+        </div>
+    )
+  }
+}
 
 class IslandSnow extends React.Component {
 
   render() {
     return (
-        <Container textAlign="center">
-    <Header as='h1'>Island Snow!</Header>
-    </Container>
-  );
+        <div>
+          <TopMenu/>
+          <IslandSnowLogo/>
+          <MiddleMenu/>
+          <FullWidthImage/>
+          <FooterMenu/>
+        </div>
+    );
   }
 }
 
-ReactDOM.render(<IslandSnow/>, document.getElementById('root'));
+ReactDOM.render(
+    <IslandSnow/>
+    , document.getElementById('root'));
